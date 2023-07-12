37864140b24001d194e1b58e8920f8e700270272
Grigory Griko <griko1996@gmail.com>
2023-07-12 09:48:31 +0300

    feat: написан TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 import UserList from './components/UserList';
 
 const App = () => {
   return (
-    <UserList></UserList>
+    <>
+      <UserList/>
+      <hr/>
+      <TodoList/>
+    </>
   )
 }
 
